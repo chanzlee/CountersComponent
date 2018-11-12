8c100e4adf5a74fa4174770afc26536dd4427593
Chan Lee <chanethanlee@gmail.com>
2018-11-11 23:17:06 -0800

    render dynmic class and render list from state list prop.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
     // imageUrl: "https://picsum.photos/200"
   };
 
@@ -13,15 +14,31 @@ class Counter extends Component {
 
   render() {
     return (
-      <React.Fragment>
+      // <React.Fragment>
+      <div>
         {/* <img src={this.state.imageUrl} alt="" /> */}
         {/* <span style={{ fontSize: 30 }}>custom style</span> */}
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
+
         <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+      // </React.Fragment>
     );
+  }
+
+  // refactor shortcut ctrl+shift+R to extract to method
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
